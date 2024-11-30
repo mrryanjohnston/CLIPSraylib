bbc9b7fc8e08d84fd054764255dbac96add40bff
Ryan Johnston <mrryanjohnston@gmail.com>
2024-11-30 18:59:37 -0500

    refactor functionality around closing the program

--- examples/program-report-mouse-no-time.bat
+++ examples/program-report-mouse-no-time.bat
@@ -1,9 +1,7 @@
 (raylib-init-window 800 400 "Hello, mouse")
 
-(deffunction check-if-should-exit (?delta)
-	(raylib-window-should-close))
-
 (deffacts setup
+	(should-exit FALSE)
 	(new-mouse-delta 0 0)
 	(new-mouse-position 0 0)
 	(new-mouse-button FALSE FALSE))
@@ -13,14 +11,15 @@
 	?p <- (new-mouse-position ?npx ?npy)
 	?c <- (new-mouse-color ?color)
 	?b <- (new-mouse-button ? ?)
-	(not (test (check-if-should-exit ?d)))
+	?s <- (should-exit FALSE)
 	=>
-	(retract ?d ?p ?c ?b)
+	(retract ?d ?p ?c ?b ?s)
 	(raylib-begin-drawing)
 		(raylib-clear-background RAYWHITE)
 		(raylib-draw-circle (integer ?npx) (integer ?npy) 100 ?color)
 	(raylib-end-drawing)
 	(assert
+		(should-exit (raylib-window-should-close))
 		(new-mouse-delta (raylib-get-mouse-delta))
 		(new-mouse-position (raylib-get-mouse-position))
 		(new-mouse-button
@@ -48,8 +47,7 @@
 	(assert (new-mouse-color PURPLE)))
 
 (defrule die
-	?d <- (new-mouse-delta ?dx ?dy)
-	(test (check-if-should-exit ?d))
+	(should-exit TRUE)
 	=>
 	(raylib-close-window)
 	(println "Closing program...")
